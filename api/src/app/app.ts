3b955f898563dc70dd30d7be28406dc32b278741
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-09 17:36:18 +0000

    Auth(Token) created api

--- api/src/app/app.ts
+++ api/src/app/app.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import appRouter from '../routes/index.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const app = express()
 
 //middlewares 
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 //only for dev 
 app.use(morgan('dev'))
--- api/src/controllers/user-controllers.ts
+++ api/src/controllers/user-controllers.ts
@@ -1,5 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt'
+import { createToken } from "../utils/token-manager.js"
+
 
 export const getAllUsers = async (req, res, next) => {
 
@@ -22,6 +24,24 @@ export const userSignup = async (req, res, next) => {
         const hashedPassword = bcrypt.hashSync(password, 10)
         const user = new User({ name, email, password: hashedPassword }) 
         await user.save()
+
+        res.clearCookie("auth_token", {
+            path: '/',
+            httpOnly: true,
+            signed:true
+        })
+
+
+        const token = createToken(user._id.toString() , user.email , "7d")
+        const expires = new Date()
+        expires.setDate(expires.getDate() + 7)
+        res.cookie("auth_token", token, {
+            path: '/',
+            expires,
+            httpOnly: true,
+            signed: true
+        })
+
         res.status(201).json({ message :'User Created Successfully!' , id: user._id.toString()})
     } catch (error) {
         res.json({error: error.message})
@@ -36,9 +56,27 @@ export const userLogin = async (req, res, next) => {
         if (!user) return res.status(401).json({ message: " User not registered" })
         
         const validPassword =  bcrypt.compareSync(password, user.password)
-        if(!validPassword) return res.status(403).json({ message: "Invalid Password" })
+        if (!validPassword) return res.status(403).json({ message: "Invalid Password" })
         
-        res.status(200).json({ message :'User Created Successfully!' , id: user._id.toString()})
+        
+        res.clearCookie("auth_token", {
+            path: '/',
+            httpOnly: true,
+            signed:true
+        })
+
+
+        const token = createToken(user._id.toString() , user.email , "7d")
+        const expires = new Date()
+        expires.setDate(expires.getDate() + 7)
+        res.cookie("auth_token", token, {
+            path: '/',
+            expires,
+            httpOnly: true,
+            signed: true
+        })
+
+        res.status(200).json({ message: 'User Logged in Successfully!', id: user._id.toString() })
     } catch (error) {
         res.json({error: error.message})
     }
