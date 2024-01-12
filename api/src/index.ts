354b9915a89b5be8902912af6e9eb3f9bf98cc7b
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-12 12:59:04 +0000

    changed bk

--- api/src/index.ts
+++ api/src/index.ts
@@ -35,9 +35,9 @@ app.use(morgan('dev'))
 
 app.use('/api/v1', appRouter)
 
-app.use(express.static(path.join(__dirname, '/client')))
+app.use(express.static(path.join(__dirname, '/client/dist')))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 
