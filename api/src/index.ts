8cf976af71de52a39e1377024600bc3e12b9fa7b
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-12 11:43:22 +0000

    latest update for rendering + setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,17 @@
-import app from "./app/app.js";
+import path from "path";
 import { connectToDB, disconnectDB } from './db/connection.js'
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import appRouter from './routes/index.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
+dotenv.config()
+
+//connection to port 
+const app = express()
 
 //db connection 
 connectToDB()
@@ -9,6 +20,26 @@ connectToDB()
     })
     .catch((e) => console.log(e))
 
+    export const __dirname = path.resolve()
 
+
+
+//middlewares 
+app.use(cors({origin: ["http://127.0.0.1:5173", 'https://my-GPT.onrender.com/'] ,credentials: true}))
+app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
+//only for dev 
+app.use(morgan('dev'))
+
+app.use('/api/v1', appRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+
+export default app
 
 
